d9a1a9bc445d047e84cbf041f64ce6a20d10aa75
ena jahic <enajahicc@gmail.com>
2020-05-11 16:53:45 +0200

    first try of users-model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_05_11_141642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.integer "price"
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.bigint "butlers_id", null: false
+    t.index ["butlers_id"], name: "index_bookings_on_butlers_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "butlers", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.integer "age"
+    t.integer "years_of_experience"
+    t.string "picture"
+    t.string "academic_grade"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "user_name"
+    t.string "password"
+    t.string "e_mail"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "bookings", "butlers", column: "butlers_id"
+  add_foreign_key "bookings", "users"
 end
