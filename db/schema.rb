9e1add5a27d0b08d92a384cf07bf8e803b7f39c2
ena jahic <enajahicc@gmail.com>
2020-05-11 20:44:57 +0200

    added picture to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_11_141642) do
+ActiveRecord::Schema.define(version: 2020_05_11_184321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2020_05_11_141642) do
     t.string "e_mail"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "picture"
   end
 
   add_foreign_key "bookings", "butlers", column: "butlers_id"
---
+++ test/models/add_picture_to_user_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class AddPictureToUserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
