2294c875c7ab8651fbd6a19f3116592749bde9fa
ena jahic <enajahicc@gmail.com>
2020-05-12 13:14:37 +0200

    I have delete mistake from user

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,6 @@ class User < ApplicationRecord
     validates :password, presence: true
     validates :first_name, presence: true
     validates :photo, presence: true
-    validates :email, presence: true, uniqueness: true
     validates :encrypted_password, presence: true
 
     devise :database_authenticatable, :registerable,
