c91c9eecf0f711a1fa16b14a79a02116354451ee
ena jahic <enajahicc@gmail.com>
2020-05-12 16:48:09 +0200

    first try of home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
+    @top_batlers = Butler.all.first(6) 
+    @batlers = Butler.all
+  end
+  def search
+    @butlers = Butler.where("name LIKE ?", "%#{params[:keyword]}%" )
   end
 end
