4f27854fd8a426cb9a83598560c8690c7a32c0ee
Andrea Goetz <gtz.andrea@gmail.com>
2020-05-14 19:05:22 +0200

    Added booking actions, general styling corrections

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,11 +3,7 @@ class DashboardsController < ApplicationController
   def index
     # @bookings = Booking.all
     @bookings = Booking.where(user_id: current_user.id)
-    @past_bookings = []
-    @upcoming_bookings = []
-    @bookings.each do |b|
-     @past_bookings << b if b.end_date < DateTime.now.in_time_zone
-     @upcoming_bookings << b if b.end_date > DateTime.now.in_time_zone
-    end
+    @past_bookings = @bookings.where("end_date < ?", DateTime.now.in_time_zone)
+    @upcoming_bookings = @bookings.where("end_date >= ?", DateTime.now.in_time_zone)
   end
 end
